e04f75f532a377ce15176fdd422734d34f56fe6a
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-20 16:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+let ans = "";
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    ans += "FizzBuzz\n";
+  } else if (i % 3 === 0) {
+    ans += "Fizz\n";
+  } else if (i % 5 === 0) {
+    ans += "Buzz\n";
+  } else {
+    ans += i + "\n";
+  }
+}
+
+console.log(ans);
